99fb3f5ca240f377df6ec43bf15a9c8022b00861
Pedro Henrique <dg.pedromarques@gmail.com>
2021-01-19 22:59:17 -0300

    Início das rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { BrowserRouter as Router, Redirect, Route, Switch } from "react-router-dom";
+
+import Login from './pages/login'
 
 function App() {
   return (
-    <div>E lá vamos nós!</div>
+    <Router>
+      <Switch>
+          <Route exact path="/login">
+              <Login></Login>
+          </Route>
+
+          <Route exact path="/">
+              <Redirect to="/login" />
+          </Route>
+
+          <Route path="*">
+              <h1>Erro 404</h1>
+          </Route>
+      </Switch>
+  </Router>
   );
 }
 
---
+++ src/themes/colors.js
@@ -0,0 +1,12 @@
+export default colors = {
+    white:              '#d8d8d8',
+    whiteTwo:           '#ffffff',
+    darkIndigo:         '#1a0e49',
+    warmGrey:           '#8d8c8c',
+    charcoalGreyTwo:    '#403e4d',
+    greyish:            '#b5b4b4',
+    mediumPink:         '#ee4c77',
+    charcoalGrey:       '#383743',
+    black54:            'rgba(0, 0, 0, 0.54)',
+    nightBlue:          '#0d0430'
+}
